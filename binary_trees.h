5cb75cc1875d3ae6d7909b3906dbcb54db776b25
Koralys Figueroa <koralys12@yahoo.com>
2025-12-10 10:12:50 -0500

    hola

--- binary_trees.h
+++ binary_trees.h
@@ -12,25 +12,62 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
-/* Prototypes */
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/* Task 0 - New node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 1 - Insert left */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2 - Insert right */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3 - Delete tree */
 void binary_tree_delete(binary_tree_t *tree);
+
+/* Task 4 - Is leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* Task 5 - Is root */
 int binary_tree_is_root(const binary_tree_t *node);
+
+/* Task 6 - Pre-order traversal */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 7 - In-order traversal */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 8 - Post-order traversal */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 9 - Height */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* Task 10 - Depth */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* Task 11 - Size */
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/* Task 12 - Leaves */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/* Task 13 - Nodes with at least 1 child */
 size_t binary_tree_nodes(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
+
+/* Provided print function (for visualization, optional) */
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
