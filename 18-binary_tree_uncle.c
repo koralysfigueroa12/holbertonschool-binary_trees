cba763b7ef57f5d18d031b573628ad30a2545475
Koralys Figueroa <koralys12@yahoo.com>
2025-12-11 12:07:25 -0500

    hola

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node in a binary tree
+ * @node: Pointer to the node to find the uncle for
+ * Return: Pointer to the uncle node, or NULL if none
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (!node || !node->parent || !node->parent->parent)
+        return (NULL);
+
+    if (node->parent->parent->left == node->parent)
+        return (node->parent->parent->right);
+    else
+        return (node->parent->parent->left);
+}
